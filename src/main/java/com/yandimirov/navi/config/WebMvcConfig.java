ddd179277d75697a2d75a7f5cf730a93143b216a
Mikhail Iandimirov <miia0217@netcracker.com>
2017-07-27 17:28:58 +0300

    Fix WebMvcConfig

--- src/main/java/com/yandimirov/navi/config/WebMvcConfig.java
+++ src/main/java/com/yandimirov/navi/config/WebMvcConfig.java
@@ -9,26 +9,29 @@ import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter{
 
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters){
-        List<Class> requestViewsList = Arrays.asList(RequestView.class.getDeclaredClasses());
-        requestViewsList.forEach(clazz -> {
-            MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-            ObjectMapper mapper = new ObjectMapper(){
-                @Override
-                protected DefaultSerializerProvider _serializerProvider(SerializationConfig config){
-                    return super._serializerProvider(config.withView(clazz));
-                }
-            };
-            mapper.configure(MapperFeature.DEFAULT_VIEW_INCLUSION, true);
-            converter.setObjectMapper(mapper);
-            converters.add(converter);
-        });
+        Stream.of(RequestView.class.getDeclaredClasses())
+            .sequential()
+            .map(clazz -> {
+                MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+                ObjectMapper mapper = new ObjectMapper(){
+                    @Override
+                    protected DefaultSerializerProvider _serializerProvider(SerializationConfig config){
+                        return super._serializerProvider(config.withView(clazz));
+                    }
+                };
+                mapper.configure(MapperFeature.DEFAULT_VIEW_INCLUSION, true);
+                converter.setObjectMapper(mapper);
+                return converter;
+            })
+            .collect(Collectors.toCollection(() -> converters));
     }
 }
